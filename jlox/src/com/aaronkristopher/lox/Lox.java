2fdfe3733f6c187760f1e8f84f359ba7c690cd37
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-02-03 08:42:43 +0800

    Add Error handling methods and helper methods

--- jlox/src/com/aaronkristopher/lox/Lox.java
+++ jlox/src/com/aaronkristopher/lox/Lox.java
@@ -10,20 +10,24 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
             System.out.println(64);
-        }
-        else if (args.length == 1) {
+        } else if (args.length == 1) {
             runFile(args[0]);
-        }
-        else {
+        } else {
             runPrompt();
         }
     }
 
     private static void runFile(String path) throws IOException {
+
+        if (hadError)
+            System.exit(65);
+
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
     }
@@ -35,7 +39,8 @@ public class Lox {
         for (;;) {
             System.out.println("> ");
             String line = reader.readLine();
-            if (line == null) break;
+            if (line == null)
+                break;
             run(line);
         }
     }
@@ -48,4 +53,15 @@ public class Lox {
             System.out.println(token);
         }
     }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
 }
