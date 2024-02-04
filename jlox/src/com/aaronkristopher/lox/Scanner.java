24306aacb59aa49bb0795590d8ef1925054bc25f
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-02-04 15:01:33 +0800

    Implement parser for keywords and identifiers

--- jlox/src/com/aaronkristopher/lox/Scanner.java
+++ jlox/src/com/aaronkristopher/lox/Scanner.java
@@ -10,6 +10,28 @@ import static com.aaronkristopher.lox.TokenType.*;
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+
+    private static final Map<String, TokenType> keywords;
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("and", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+
     private int start = 0;
     private int current = 0;
     private int line = 1;
@@ -25,7 +47,7 @@ public class Scanner {
             scanToken();
         }
 
-        tokens.add(newToken(EOF, "", null, line));
+        tokens.add(new Token(EOF, "", null, line));
         return tokens;
     }
 
@@ -99,10 +121,35 @@ public class Scanner {
             default:
                 if (isDigit(c))
                     number();
+                else if (isAlpha(c))
+                    identifier();
                 else
                     Lox.error(line, "Unexpected character.");
                 break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null)
+            type = IDENTIFIER;
+        addToken(type);
+
+        addToken(IDENTIFIER);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
